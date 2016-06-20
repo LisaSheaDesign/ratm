9c8d6f01db2aba2e251942f6fbfa3fc70563513e
LisaSheaDesign <LisaSheaDesign@gmail.com>
2016-06-19 23:25:52 -0700

    update now play and photo js

--- includes/nowplay.php
+++ includes/nowplay.php
@@ -6,9 +6,8 @@
 	<!-- Place all images and links below for slideshow -->
 		<div class="my-gallery swipeshow">
   			<ul class="slides">
-    			<li class="slide"><a href="https://www.facebook.com/events/1617878455095427/" target="_blank"><img src="img/slideshow/InstaGator8-01.jpg" border="0" alt="Gator"/></a></li>
-		    	<li class="slide"><img src="img/slideshow/hideout-may-14-03.jpg" border="0" alt="Hide Out"/></li>
-				<li class="slide"><a href="https://www.facebook.com/events/434997396669711/" target="_blank"><img src="img/slideshow/710-MAY-16-02.jpg" border="0" alt="Beach Club"/></a></li>
+    			<li class="slide"><a href="https://www.facebook.com/RatMmusic/"target="_blank"><img src="img/slideshow/woodstock-jun-24-25-26-16.jpg" border="0" alt="Woodstock June 24 25 26 2016"/></a></li>
+				<li class="slide"><a href="hhttps://www.facebook.com/events/555832277922645/" target="_blank"><img src="img/slideshow/710-jun-10-16.jpg" border="0" alt="710 Beach Club June 10 2016"/></a></li>
 			</ul>
 			 <div class='dots'></div>
 		</div> <!-- my-gallery END -->
